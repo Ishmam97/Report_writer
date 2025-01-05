493dd763c8d3e508c62473c3b1489b7db3af6bd7
Ishmam <ishmam.a.solaiman@gmail.com>
2025-01-05 02:48:09 -0600

    add streamlit components to run graph and display outputs

--- report_writer/app.py
+++ report_writer/app.py
@@ -8,7 +8,11 @@ from tavily import TavilyClient
 from typing import TypedDict, List
 import prompts
 from models import get_models
+from graph import build_graph
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 def main():
     st.title("Report Writing Assistant")
     st.write("Researches a topic and writes a report on it.")
@@ -31,13 +35,15 @@ def main():
         st.warning("Please enter both your OpenAI and Tavily API keys to proceed.")
         st.stop()  # Prevents the rest of the app from running
 
-    # Initialize the ChatOpenAI model after validating API keys
-    model = "ollama"
-    llm = get_models(model, openai_api_key)
-
     # User Inputs for Report Generation
     st.header("Report Details")
     topic = st.text_input("Enter your research topic:")
+    
+    # Initialize the ChatOpenAI model after validating API keys
+    model = "ollama"
+    llm = get_models(model, openai_api_key)
+    graph = build_graph(llm, tavily_api_key)
+    
 
     # Check if name and topic are provided
     if not topic:
@@ -46,8 +52,39 @@ def main():
 
     # Submit Button
     if st.button("Submit"):
-        # You can add more logic here to handle the report generation
-        st.success(f"Your report on '{topic}' is being generated...")
+       try:
+        initial_state = {
+            "task": f"Write a report about {topic}",
+            "max_revisions": 2,
+            "revision_number": 1,
+            "plan": "",
+            "draft": "",
+            "critique": "",
+            "content": [],
+        }
+        thread = {"configurable":{"thread_id": "1"}}
+        for state in graph.stream(initial_state, thread):
+            key = list(state.keys())[0]
+            if key == 'planner':
+                st.header('planner:')
+                st.write(state['planner']['plan'])
+            elif key == 'researcher':
+                st.header('researcher:')
+                for content in state['researcher']['content']:
+                    st.write(content)
+            elif key == 'generate':
+                st.header('generate:')
+                st.write(state['generate']['draft'])
+            elif key == 'reflect':
+                st.header('reflect:')
+                st.write(state['reflect']["critique"])
+            elif key == 'research_critique':
+                st.header('research_critique:')
+                for content in state['research_critique']['content']:
+                    st.write(content)
 
+       except Exception as e:
+            logger.error(f"An error occurred: {e}", exc_info=True, stack_info=True)
+            st.error(f"An error occurred: {e}")
 if __name__ == "__main__":
     main()
